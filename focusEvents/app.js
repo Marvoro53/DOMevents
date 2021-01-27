bd77f9df777d0fc015eee2fcb3d643d75bed2d69
Marvoro53 <marvinlara321@gmail.com>
2021-01-26 20:25:27 -0600

    finished exercise

--- focusEvents/app.js
+++ focusEvents/app.js
@@ -1,3 +1,11 @@
 // When you enter the input field, a function is triggered which sets the background 
 // color to skyBlue. When you leave the input field, 
 // a function is triggered which sets the background color to grey.
+var color= document.getElementById('name')
+addEventListener('focus', (event) => {
+    event.target.style.background = 'yellow';
+  }, true);
+
+  addEventListener('blur', (event) => {
+    event.target.style.background = '';
+  }, true);
