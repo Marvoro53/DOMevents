28e89ff4ac8ab70e2be12e51157d6e84315a1cc1
Marvoro53 <marvinlara321@gmail.com>
2021-01-26 21:52:11 -0600

    finished exercise

--- MutationEvents/app.js
+++ MutationEvents/app.js
@@ -1,3 +1,17 @@
 //Click the buttons to toggle between showing and hiding the content in collapsible.
+var column= document.getElementsByClassName('collapsible');
+var i;
 
+for(let i=0; i<column.length; i++){
+    column[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+        var content = this.nextElementSibling;
+        if(content.style.maxHeight){
+            content.style.maxHeight=null;
+        }
+        else{
+            content.style.maxHeight= content.scrollHeight + 'px';
+        }
+    })
+}
 
